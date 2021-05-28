ac4db9d4abbbe758eb3c72f14be0639b10adc266
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2021-05-28 17:52:25 +0200

    Add Disabled HOC & Alert in case EULA not accepted

---
+++ js/src/utils/DisabledIfNotAccepted.js
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2021 CZ.NIC z.s.p.o. (https://www.nic.cz/)
+ *
+ * This is free software, licensed under the GNU General Public License v3.
+ * See /LICENSE for more information.
+ */
+
+import React from "react";
+import PropTypes from "prop-types";
+
+import DisabledAlert from "./DisabledAlert";
+
+DisabledIfNotAccepted.propTypes = {
+    formData: PropTypes.shape({ eula: PropTypes.number }),
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node,
+    ]),
+};
+
+export default function DisabledIfNotAccepted({
+    children,
+    formData,
+    ...props
+}) {
+    const isDisabled = formData.eula !== 1;
+    const childrenWithFormProps = React.Children.map(children, (child) =>
+        React.cloneElement(child, { ...props, formData, disabled: isDisabled })
+    );
+
+    if (!isDisabled) return childrenWithFormProps;
+
+    return (
+        <>
+            <DisabledAlert />
+            <span className="text-muted">{childrenWithFormProps}</span>
+        </>
+    );
+}
