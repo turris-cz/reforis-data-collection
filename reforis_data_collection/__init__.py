75e974861acdf1642ec79b94929c63abe5af7008
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2024-03-07 11:22:41 +0100

    Format Python files with Ruff

--- reforis_data_collection/__init__.py
+++ reforis_data_collection/__init__.py
@@ -1,4 +1,4 @@
-#  Copyright (C) 2020-2021 CZ.NIC z.s.p.o. (http://www.nic.cz/)
+#  Copyright (C) 2020-2024 CZ.NIC z.s.p.o. (https://www.nic.cz/)
 #
 #  This is free software, licensed under the GNU General Public License v3.
 #  See /LICENSE for more information.
@@ -14,47 +14,47 @@ from flask_babel import gettext as _
 from reforis.foris_controller_api.utils import validate_json, APIError
 
 blueprint = Blueprint(
-    'DataCollection',
+    "DataCollection",
     __name__,
-    url_prefix='/data-collection/api',
+    url_prefix="/data-collection/api",
 )
 
 BASE_DIR = Path(__file__).parent
 
 data_collection = {
-    'blueprint': blueprint,
+    "blueprint": blueprint,
     # Define {python_module_name}/js/app.min.js
     # See https://gitlab.labs.nic.cz/turris/reforis/reforis-distutils/blob/master/reforis_distutils/__init__.py#L11
-    'js_app_path': 'reforis_data_collection/js/app.min.js',
-    'translations_path': BASE_DIR / 'translations',
+    "js_app_path": "reforis_data_collection/js/app.min.js",
+    "translations_path": BASE_DIR / "translations",
 }
 
 
-@blueprint.route('/settings', methods=['GET'])
+@blueprint.route("/settings", methods=["GET"])
 def get_settings():
-    """ Get sentinel settings """
-    return jsonify(current_app.backend.perform('sentinel', 'get_settings'))
+    """Get sentinel settings"""
+    return jsonify(current_app.backend.perform("sentinel", "get_settings"))
 
 
-@blueprint.route('/settings', methods=['POST'])
+@blueprint.route("/settings", methods=["POST"])
 def post_settings():
-    """ Update sentinel settings """
-    validate_json(request.json, {'eula': int})
+    """Update sentinel settings"""
+    validate_json(request.json, {"eula": int})
 
-    response = current_app.backend.perform('sentinel', 'update_settings', request.json)
-    if response.get('result') is not True:
-        raise APIError(_('Cannot update Sentinel settings'), HTTPStatus.INTERNAL_SERVER_ERROR)
+    response = current_app.backend.perform("sentinel", "update_settings", request.json)
+    if response.get("result") is not True:
+        raise APIError(_("Cannot update Sentinel settings"), HTTPStatus.INTERNAL_SERVER_ERROR)
 
     return jsonify(response), HTTPStatus.NO_CONTENT
 
 
-@blueprint.route('/eula', methods=['GET'])
+@blueprint.route("/eula", methods=["GET"])
 def get_eula():
-    """ Get sentinel EULA """
-    return jsonify(current_app.backend.perform('sentinel', 'get_eula'))
+    """Get sentinel EULA"""
+    return jsonify(current_app.backend.perform("sentinel", "get_eula"))
 
 
-@blueprint.route('/state', methods=['GET'])
+@blueprint.route("/state", methods=["GET"])
 def get_state():
-    """ Get sentinel components state """
-    return jsonify(current_app.backend.perform('sentinel', 'get_state'))
+    """Get sentinel components state"""
+    return jsonify(current_app.backend.perform("sentinel", "get_state"))
